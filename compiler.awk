e99d228652320ef06afa36c988941233c25b5873
duzenko <anton.duzenko@gmail.com>
2023-07-23 12:32:14 +0300

    if operator

--- compiler.awk
+++ compiler.awk
@@ -2,23 +2,23 @@ BEGIN {
     print "#!/bin/bash\n"
 }
 
-function MapIdentifier(s) {
-    if(s=="WriteLn") return "echo"
-    if(s=="ReadLn") return "read"
-    return s
-}
-
 {
     delete parts
     for(i=1; i<=NF; i++) {
         parts[length(parts)] = $i
     }
     for(i=0; i<length(parts); i++) {
-        mapped = MapIdentifier(parts[i])
+		s = parts[i]
+		if(s=="WriteLn") s = "echo"
+		else if(s=="ReadLn") s = "read"
+		else if(s=="if") {
+			s = "if [ -n"
+			parts[length(parts)] = "] \nthen"
+		}
         if(i)
-            output = output " " mapped
+            output = output " " s
         else
-            output = mapped
+            output = s
     } 
     print output
 }
--- run.sh
+++ run.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -eo pipefail
 
+TEST_DIR=tests/01-hello-world
 TEST_DIR=tests/02-read-string
+TEST_DIR=tests/03-if
 
-echo Compiling $TEST_DIR/test.qwe...
-awk -v FPAT='"[^"]*"|[^ ]*' -f compiler.awk $TEST_DIR/test.qwe > $TEST_DIR/test.sh
+for TEST_DIR in tests/*
+do
+    echo $TEST_DIR
+#    echo Compiling $TEST_DIR/test.qwe...
+    awk -v FPAT='"[^"]*"|[^ ]*' -f compiler.awk $TEST_DIR/test.qwe > $TEST_DIR/test.sh
 
-if [ -f "$TEST_DIR/input.txt" ]
-then
-    echo Testing with input file...
-    cat $TEST_DIR/input.txt | $TEST_DIR/test.sh
-else
-    echo Testing...
-    $TEST_DIR/test.sh
-fi
+    for f in $TEST_DIR/input*.txt
+    do
+        if [ -f "$f" ]
+        then
+#            echo Testing with input file $f...
+            cat $f | $TEST_DIR/test.sh
+            TESTED=true
+        else
+#            echo Testing...
+            $TEST_DIR/test.sh
+        fi
+    done
+    echo
+done
